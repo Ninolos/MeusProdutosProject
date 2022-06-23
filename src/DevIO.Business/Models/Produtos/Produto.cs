0ae223bef882eb9e31eafbea9f753f56361d39c8
Jonathan Loth <ninoloth@gmail.com>
2022-06-23 18:03:11 -0300

    Escrevendo entidades iniciais

--- src/DevIO.Business/Models/Produtos/Produto.cs
+++ src/DevIO.Business/Models/Produtos/Produto.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Business.Core.Models;
+using DevIO.Business.Models.Fornecedores;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,15 @@ namespace DevIO.Business.Models.Produtos
 {
     public class Produto : Entity
     {
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
 
+        //Relação EF
+        public Fornecedor Fornecedor { get; set; }
     }
 }

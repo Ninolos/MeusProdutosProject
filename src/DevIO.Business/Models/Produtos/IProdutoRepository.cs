92ed34b352b1b6ecc663cd672050cfd5d1cc382b
Jonathan Loth <ninoloth@gmail.com>
2022-06-23 18:23:43 -0300

    Interfaces de acessos a dados

---
+++ src/DevIO.Business/Models/Produtos/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.Business.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Produtos
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutoPorFornecedor(Guid fornecedorId); //obtem prod por fornecedor
+        Task<IEnumerable<Produto>> ObterProdutoFornecedores(); // obtem tudo
+        Task<Produto> ObterProdutoFornecedor(Guid id); //obtem produto
+    }
+}

6dabef9565ed7548bf862e5524ffc84c41cf6be4
Jonathan Loth <ninoloth@gmail.com>
2022-06-24 13:31:47 -0300

    validaçao documento

--- src/DevIO.Business/Models/Fornecedores/TipoFornecedor.cs
+++ src/DevIO.Business/Models/Fornecedores/TipoFornecedor.cs
@@ -3,7 +3,7 @@
     public enum TipoFornecedor
     {
         PessoaFisica = 1,
-        pessoaJuridica = 2,
+        PessoaJuridica = 2,
     }
 
 }
---
+++ src/DevIO.Business/Models/Fornecedores/Validations/FornecedorValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Fornecedores.Validations
+{
+    public class FornecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(f => f.Nome)
+                .NotEmpty()
+                .WithMessage("O Campo {PropertyName} precisa ser fornecido")
+                .Length(2, 200)
+                .WithMessage("O Campo {PropertyName} precisa ser entre {MinLength} e {MaxLength}");
+
+            When(f => f.TipoFornecedor == TipoFornecedor.PessoaFisica, () =>
+            {
+
+            });
+
+            When(f => f.TipoFornecedor == TipoFornecedor.PessoaJuridica, () =>
+            {
+
+            });
+        }
+    }
+}

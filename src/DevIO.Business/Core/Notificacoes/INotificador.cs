31d0b8efb2f1d5d48a0599d1b5b4ecdbc48d8d8f
Jonathan Loth <ninoloth@gmail.com>
2022-06-29 22:06:22 -0300

    Iniciando as Notificações

---
+++ src/DevIO.Business/Core/Notificacoes/INotificador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Core.Notificacoes
+{   
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacao();
+        void Handle(Notificacao notificacao);
+    }
+}
---
+++ src/DevIO.Business/Core/Notificacoes/Notificador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Core.Notificacoes
+{
+    public class Notificador : INotificador
+    {
+        private List<Notificacao> _notificacaos;
+
+        public Notificador()
+        {
+            _notificacaos = new List<Notificacao>();
+        }
+        public void Handle(Notificacao notificacao)
+        {
+            _notificacaos.Add(notificacao);
+        }
+
+        public List<Notificacao> ObterNotificacao()
+        {
+            return _notificacaos;
+        }
+
+        public bool TemNotificacao()
+        {
+            return _notificacaos.Any();    
+        }
+    }
+}
